af1bd8cb0095546a1dbc175dfa5bc5482b8b8a7a
Jacob Kim <jacob@mykoon.com>
2019-11-05 18:39:59 +0900

    hello splitting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+// import LoginPage from "./LoginPage";
+// import TestPage from "./TestPage";
+
+const LoginPage = React.lazy(() => import("./LoginPage"));
+const TestPage = React.lazy(() => import("./TestPage"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>Weclome to code spliting class with lazy</div>
+      <Link to="/login">login</Link>
+      <Link to="/test">test</Link>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Route exact path="/login" component={LoginPage} />
+        <Route path="/test" component={TestPage} />
+      </React.Suspense>
+    </BrowserRouter>
   );
 }
 
